2844ebaf68433ac85a1c6205c285c2580a8fbe43
oways-work <owayswork@gmail.com>
2026-03-14 22:14:29 +0300

    Update 6-sky_high.js

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -6,14 +6,10 @@ export default class SkyHighBuilding extends Building {
     this._floors = floors;
   }
 
-  get sqft() {
-    return this._sqft;
-  }
-
   get floors() {
     return this._floors;
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this.floors} floors.`;
   }
