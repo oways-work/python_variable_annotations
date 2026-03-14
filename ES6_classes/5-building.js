3fdbcf628e05cc20b6d8175b05e0e0266c8a893f
oways-work <owayswork@gmail.com>
2026-03-14 22:14:08 +0300

    Update 5-building.js

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,10 +1,9 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
-    // Ensure subclasses implement evacuationWarningMessage
     if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+    this._sqft = sqft;
   }
 
   get sqft() {
